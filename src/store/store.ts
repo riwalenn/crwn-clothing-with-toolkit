f0910880eb5e2ccabe275feefc0953a65401d493
Riwalenn Bas <riwalenn@gmail.com>
2024-07-19 10:01:59 +0200

    Typescript Advanced: Redux Store + Middleware - fix saga error

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,17 +8,17 @@ import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
 
 declare global {
   interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?: typeof compose
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
 
 type ExtendedPersistConfig = PersistConfig<RootState> & {
   whitelist: (keyof RootState)[];
-}
+};
 
 const persistConfig: ExtendedPersistConfig = {
   key: 'root',
@@ -33,20 +33,20 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleWares = [
   process.env.NODE_ENV !== 'production' && logger,
   sagaMiddleware,
-].filter((middleware): middleware is Middleware => Boolean());
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+    (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
+    persistedReducer,
+    undefined,
+    composedEnhancers
 );
 
 sagaMiddleware.run(rootSaga);
